141cc79e0499e2a1439c735c9a307b9b7de2a26e
bergandara <gandaraber@gmail.com>
2024-06-18 14:39:48 +0100

    Create StudentResponse.java

---
+++ student-service/src/main/java/com/hexagon/student_service/dto/StudentResponse.java
@@ -0,0 +1,18 @@
+package com.hexagon.student_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentResponse {
+    private String id;
+    private String name;
+    private int age;
+    private String gender;
+    private School school;
+}
