6e2a3855966ed12796467d780496f3bb181d57e6
bergandara <gandaraber@gmail.com>
2024-06-19 01:03:13 +0100

    Create StudentController.java

---
+++ student-service/src/main/java/com/hexagon/student_service/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.hexagon.student_service.controller;
+
+import com.hexagon.student_service.entity.Student;
+import com.hexagon.student_service.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> fetchStudentById(@PathVariable String id){
+        return studentService.fetchStudentById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> fetchStudents(){
+        return studentService.fetchStudents();
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> createStudent(@RequestBody Student student){
+        return studentService.createStudent(student);
+    }
+}
